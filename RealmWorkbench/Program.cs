0500c473e8b586f143241086dc149dd5258b94d4
jvyden <jvyden@jvyden.xyz>
2023-09-04 16:47:03 -0400

    Print out schema

--- RealmWorkbench/Program.cs
+++ RealmWorkbench/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Realms;
+using Realms.Schema;
 
-Console.WriteLine("Hello, World!");
+string path = string.Join(' ', args);
+Console.Write("Path: ");
+Console.WriteLine(path);
+
+RealmConfiguration configuration = new RealmConfiguration(path)
+{
+    IsDynamic = true,
+    ShouldDeleteIfMigrationNeeded = false,
+    IsReadOnly = true,
+};
+
+Realm realm = Realm.GetInstance(configuration);
+
+foreach (ObjectSchema schema in realm.Schema)
+{
+    Console.WriteLine($"{schema.Name} ({schema.Count})");
+    foreach (Property property in schema)
+    {
+        string type = string.IsNullOrEmpty(property.ObjectType) ? property.Type.ToString() : property.ObjectType;
+        if ((property.Type & PropertyType.Array) != 0) type += "[]";
+        Console.WriteLine($"  {property.Name}: {type}");
+    }
+}
